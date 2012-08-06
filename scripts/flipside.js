56ae2bbd0550dd2524c82f1a87b0d2ee249dcd99
Olaf Veerman <olaf.veerman@gmail.com>
2012-08-06 08:15:04 +0100

    Float the menu items instead of stacking them and tweak the contact fly-out. Fix #2 & #8

---
+++ scripts/flipside.js
@@ -0,0 +1,12 @@
+/* The contact fly-out */
+
+$(document).ready(function(){
+
+    $(".contact-info").hide();
+    $(".show_hide").show();
+	 
+    $('.show_hide').click(function(){
+        $(".contact-info").slideToggle("medium");
+    });
+
+});

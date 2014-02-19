08dc444cc534d2e03fa6ea1dc6fe3d6837419d02
Daniel Silva <daniel.silva@flipside.org>
2014-02-19 16:37:07 +0000

    Fix #170. Disables animation loop for the slider.

--- src/js/slider.js
+++ src/js/slider.js
@@ -2,6 +2,7 @@ $(document).ready(function() {
   $('.flexslider').flexslider({
     animation: 'slide',
     // http://stackoverflow.com/questions/11471034/weird-flickering-background-with-multiple-flex-sliders-on-page
-    useCSS:false
+    useCSS:false,
+    animationLoop: false,
   });
 });

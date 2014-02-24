6a46eebf9ce5581cd8ed13cfad187679d9fb8071
Daniel Silva <daniel.silva@flipside.org>
2014-02-24 17:57:01 +0000

    Fix #176. Update keypress library to latest version.

--- src/js/easter_eggs.js
+++ src/js/easter_eggs.js
@@ -1,13 +1,14 @@
 $(document).ready(function() {
   // Easter eggs.
+  var listener = new window.keypress.Listener();
   
   // Konami code
-  keypress.sequence_combo("up up down down left right left right b a", function() {
+  listener.sequence_combo("up up down down left right left right b a", function() {
     window.location = 'http://edispilf.org';
-  }, false);
+  });
   
-  keypress.sequence_combo("f l i p s i d e", function() {
+  listener.sequence_combo("f l i p s i d e", function() {
     alert('Hello! :)');
-  }, false);
+  });
   
 });

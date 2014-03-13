c7ab575f00501168358ee0c16305851e2744c5e4
Daniel Silva <daniel.silva@flipside.org>
2014-03-13 15:12:47 +0000

    Fix problem with carousel's dynamic height on chrome

--- src/js/carousel_dynamic_height.js
+++ src/js/carousel_dynamic_height.js
@@ -1,20 +1,25 @@
 // This #$%&=! needs to be window, can't be document. Everything needs to
 // be loaded otherwise size will be @#$&%! up.
 $(window).load(function() {
-  
   var $slider_li = $('.flexslider .slides li');
-  
+
   if ($slider_li.length > 1) {
     fix_slider_height();
     
     $( window ).resize(function() {
       fix_slider_height();
     });
+    
+    // Chrome has a weird behavior with the slider.
+    // When the page loads the height is not correctly computed.
+    // Wait 50ms and set again.
+    // I've no idea why this happens. If somebody finds out please fix!
+    setTimeout(function(){ fix_slider_height(); }, 50);
   }
   
   function fix_slider_height() {
     var max_height = 0;
-    
+
     // Reset everything.
     $slider_li.css('min-height', '');
     $slider_li.find('.media').css('min-height', '');
@@ -25,7 +30,7 @@ $(window).load(function() {
         max_height = $(this).outerHeight();
       }
     });
-    
+
     $slider_li.css('min-height', max_height);
     
     // The div.media must be the same height as the li minus the padding
